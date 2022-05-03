6e8112c5370af62cab70f074f9740e7475965ecf
trbrooks <tim.r.brooks@nasa.gov>
2022-05-03 16:32:05 -0400

    Adding t to parameter sweep

--- rst_test.py
+++ rst_test.py
@@ -11,11 +11,12 @@ geom_id = all_geoms[0]
 # Create a grid of test points in rst for wing
 nr = 25
 ns = 5
+nt = 3
 r_array = np.linspace(0.0, 1.0, nr)
 s_array = np.linspace(0.0, 0.5, ns)
-rst0 = np.zeros([nr, ns, 3])
-rst0[:, :, 0], rst0[:, :, 1] = np.meshgrid(r_array, s_array, indexing='ij')
-rst0[:, :, 2] = 0.5
+t_array = np.linspace(0.0, 1.0, nt)
+rst0 = np.zeros([nr, ns, nt, 3])
+rst0[:, :, :, 0], rst0[:, :, :, 1], rst0[:, :, :, 2] = np.meshgrid(r_array, s_array, t_array, indexing='ij')
 rst0 = rst0.reshape(-1, 3)
 # Find the test points xyz locations
 pts0 = vsp.CompVecPntRST(geom_id, 0, rst0[:, 0], rst0[:, 1], rst0[:, 2])
@@ -27,12 +28,16 @@ r1, s1, t1, d1 = vsp.FindRSTVec(geom_id, 0, pts0)
 pts1 = vsp.CompVecPntRST(geom_id, 0, r1, s1, t1)
 xyz1 = np.array([[pnt.x(), pnt.y(), pnt.z()] for pnt in pts1])
 
+# Print max error from projection
+print(f'Max projection error is: {max(d1)}')
+
 # Plot the test points vs projected points in xy
-plt.figure(0)
-plt.title('Wing projection test')
-plt.scatter(xyz0[:, 0], xyz0[:, 1])
-plt.scatter(xyz1[:, 0], xyz1[:, 1])
-plt.xlabel('x')
-plt.ylabel('y')
-plt.legend(['Original points', 'rst Projections'])
+fig = plt.figure(0)
+ax = fig.add_subplot(projection='3d')
+ax.set_title('Wing projection test')
+ax.scatter(xyz0[:, 0], xyz0[:, 1], xyz0[:, 2])
+ax.scatter(xyz1[:, 0], xyz1[:, 1], xyz1[:, 2])
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.legend(['Original points', 'rst Projections'])
 plt.show()
